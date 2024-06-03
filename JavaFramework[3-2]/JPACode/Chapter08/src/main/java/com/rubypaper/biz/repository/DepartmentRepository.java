c53ad1f79ed98089f7bc04afb645c75b7cf3a4b5
dlduydrb123 <dldydrb-40@naver.com>
2024-06-03 15:26:48 +0900

    re_spring_14th

--- JavaFramework[3-2]/JPACode/Chapter08/src/main/java/com/rubypaper/biz/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rubypaper.biz.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rubypaper.biz.domain.Department;
-
-public interface DepartmentRepository extends CrudRepository<Department, Long> {
-
-}
